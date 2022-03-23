869de565ef729a681fa128f6c7be8fe3e10b6413
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-23 10:20:34 +0530

    added

---
+++ WebApplicationWithSql/WebApplicationWithSql/Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationWithSql.Models;
+
+namespace WebApplicationWithSql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly SampleDbContext SampleDbContext;
+
+        public SampleController(SampleDbContext _sampleDbContext)
+        {
+            SampleDbContext = _sampleDbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<TblSample> Get()
+        {
+            return SampleDbContext.TblSamples.ToList();
+        }
+    }
+}
