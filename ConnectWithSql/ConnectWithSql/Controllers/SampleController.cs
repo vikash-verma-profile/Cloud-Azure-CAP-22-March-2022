84f67712f6e590968a8e5802eacaf2fa1720fa89
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-04-05 13:18:56 +0530

    added

---
+++ ConnectWithSql/ConnectWithSql/Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+﻿using ConnectWithSql.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectWithSql.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        SampleDBContext _sampleDBContext;
+        public SampleController(SampleDBContext sampleDBContext)
+        {
+            _sampleDBContext = sampleDBContext;
+        }
+        [HttpGet]
+        public IEnumerable<TblSample> GetData()
+        {
+            return _sampleDBContext.TblSamples.ToList();
+        }
+    }
+}
---
+++ ConnectWithSql/ConnectWithSql/Models/SampleDBContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ConnectWithSql.Models
+{
+    public partial class SampleDBContext : DbContext
+    {
+        public SampleDBContext()
+        {
+        }
+
+        public SampleDBContext(DbContextOptions<SampleDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblSample> TblSamples { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblSample>(entity =>
+            {
+                entity.ToTable("tblSample");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Text).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
