f4a52b0bc6e2bad90f096199a557dcadfa5a7f9c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-04-19 19:42:32 +0530

    first commit

---
+++ EcommerceWebApp/EcommerceWebApp/Models/EcommerceDbContext.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EcommerceWebApp.Models
+{
+    public partial class EcommerceDbContext : DbContext
+    {
+        public EcommerceDbContext()
+        {
+        }
+
+        public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCategory> TblCategories { get; set; }
+        public virtual DbSet<TblColor> TblColors { get; set; }
+        public virtual DbSet<TblProduct> TblProducts { get; set; }
+        public virtual DbSet<TblProductSizeColor> TblProductSizeColors { get; set; }
+        public virtual DbSet<TblRole> TblRoles { get; set; }
+        public virtual DbSet<TblRoleUserMapping> TblRoleUserMappings { get; set; }
+        public virtual DbSet<TblSize> TblSizes { get; set; }
+        public virtual DbSet<TblUser> TblUsers { get; set; }
+        public virtual DbSet<TblUserDetail> TblUserDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=EcommerceDb;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblCategory>(entity =>
+            {
+                entity.ToTable("tblCategory");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CategoryName).HasMaxLength(50);
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.ParentId).HasColumnName("ParentID");
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TblColor>(entity =>
+            {
+                entity.ToTable("tblColor");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.ColorName).HasMaxLength(50);
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TblProduct>(entity =>
+            {
+                entity.ToTable("tblProduct");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.Description).HasMaxLength(2000);
+
+                entity.Property(e => e.FinalPrice).HasColumnType("decimal(5, 2)");
+
+                entity.Property(e => e.Mrp).HasColumnType("decimal(5, 2)");
+
+                entity.Property(e => e.ProductName).HasMaxLength(50);
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+
+                entity.Property(e => e.VendorName)
+                    .HasMaxLength(10)
+                    .IsFixedLength(true);
+            });
+
+            modelBuilder.Entity<TblProductSizeColor>(entity =>
+            {
+                entity.ToTable("tblProductSizeColor");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TblRole>(entity =>
+            {
+                entity.ToTable("tblRole");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.RoleName).HasMaxLength(20);
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TblRoleUserMapping>(entity =>
+            {
+                entity.ToTable("tblRoleUserMapping");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TblSize>(entity =>
+            {
+                entity.ToTable("tblSize");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.SizeName).HasMaxLength(50);
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TblUser>(entity =>
+            {
+                entity.ToTable("tblUser");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+
+                entity.Property(e => e.UserName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<TblUserDetail>(entity =>
+            {
+                entity.ToTable("tblUserDetails");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.Email).HasMaxLength(200);
+
+                entity.Property(e => e.FirstName).HasMaxLength(200);
+
+                entity.Property(e => e.Gender).HasMaxLength(10);
+
+                entity.Property(e => e.LastName).HasMaxLength(200);
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EcommerceWebApp/EcommerceWebApp/Models/TblRoleUserMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EcommerceWebApp.Models
+{
+    public partial class TblRoleUserMapping
+    {
+        public int Id { get; set; }
+        public int? RoleId { get; set; }
+        public int? UserId { get; set; }
+        public int? IsDeleted { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
